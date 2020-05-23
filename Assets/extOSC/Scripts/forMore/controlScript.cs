d863333e24efd2bbb1a9ad197fba8e053fa9fff8
croyn <batchild2004@hotmail.com>
2020-05-23 18:19:54 +0200

    prefab de tio que se mueve

--- Assets/extOSC/Scripts/forMore/controlScript.cs
+++ Assets/extOSC/Scripts/forMore/controlScript.cs
@@ -6,29 +6,74 @@ public class controlScript : MonoBehaviour
 {
     public static controlScript instance;
     private int counter=0;
-    public GameObject sphere;
+    public GameObject manoIz;
+    public GameObject manoDer;
+    
+    private bool manoIzqInPosition;
+    private bool manoDerInPosition;
+
+    Vector3 pos1manoIzqJUEGO;
+    Vector3 pos1manoDerJUEGO;
+    Vector3 pos2manoIzqJUEGO;
+    Vector3 pos2manoDerJUEGO;
+    Vector3 pos3manoIzqJUEGO;
+    Vector3 pos3manoDerJUEGO;
+
 
     // Start is called before the first frame update
     void Awake()
     {
         instance = this;
+        manoIzqInPosition = false;
+        manoDerInPosition = false;
+    
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        if (counter == 6) {
-            sphere.SetActive(true);
+
+        check();
+    }
+
+    void check() {
+        if (manoDerInPosition && manoIzqInPosition)
+        {
+           
+
+            switch (counter)
+            {
+                case 0:
+                    manoIz.transform.position = pos1manoIzqJUEGO;
+                    manoDer.transform.position = pos1manoDerJUEGO;
+                    break;
+                case 1:
+                    manoIz.transform.position = pos2manoIzqJUEGO;
+                    manoDer.transform.position = pos2manoDerJUEGO;
+                    break;
+                case 2:
+                    manoIz.transform.position = pos3manoIzqJUEGO;
+                    manoDer.transform.position = pos3manoDerJUEGO;
+                    break;
+                default: break;
+            }
+
+
         }
+        
+
     }
 
+   
 
-    public void setCounter() {
-        counter++;
+    public void setManoIzqPosition(bool cual) {
+        manoIzqInPosition = cual;
     }
 
-    public int getCounter() {
-        return counter;
+    public void setManoDerPosition(bool cual)
+    {
+        manoDerInPosition = cual;
     }
 
 }
