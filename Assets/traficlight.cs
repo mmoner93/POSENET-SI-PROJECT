9bf8d8016808b34acb64a1a829f0a17c01df96d7
MMoner <mmoner93@gmail.com>
2020-05-29 14:33:54 +0200

    update

--- Assets/traficlight.cs
+++ Assets/traficlight.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class traficlight : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
